24d7aa1590f36ef77f781090747325a6b5bceb3e
Torsten Fietzek <torsten.fietzek@informatik.tu-chemnitz.de>
2026-04-16 00:12:43 +0200

    [conda] update environment variable additions

--- conda_install/prepare_icub_conda.sh
+++ conda_install/prepare_icub_conda.sh
@@ -23,18 +23,18 @@ else
     postfix="_${env_name}"
 fi
 
-conda create -y -n ${env_name}
-conda install -y -n ${env_name} -c robotology opencv yarp icub-main gazebo-yarp-plugins icub-models
+conda create -y -n ${env_name} python==3.9
+conda install -y -n ${env_name} -c robotology opencv yarp==3.8.1 icub-main gazebo-yarp-plugins icub-models
 
-if [ -z "${ICUB_INSTALL_PREFIX}" ] || [ "${ICUB_INSTALL_PREFIX}" != "${conda_path}/envs/robotologyenv" ];
+
+if [ -z "${ICUB_INSTALL_PREFIX}" ] || [ "${ICUB_INSTALL_PREFIX}" != "${conda_path}/envs/${env_name}" ];
 then
-echo "Set env variables in bashrc"
+echo "Set official iCub/YARP env variables in bashrc"
 cp ~/.bashrc $BASEDIR/.bashrc_prep_bak_$(date  "+%Y_%m_%d_%H_%M_%S")
 
 rcadditions="""
 alias iCub${postfix}='conda activate ${env_name}'
 export ICUB_INSTALL_PREFIX="$conda_path"/envs/"$env_name"
-export ICUB_TOOLDIR="$TOOLDIR"
 """
 
 rcadditions1='''
@@ -45,6 +45,25 @@ export GAZEBO_PLUGIN_PATH=${GAZEBO_PLUGIN_PATH:+${GAZEBO_PLUGIN_PATH}:}${ICUB_IN
 export GAZEBO_RESOURCE_PATH=${GAZEBO_RESOURCE_PATH:+${GAZEBO_RESOURCE_PATH}:}${ICUB_INSTALL_PREFIX}/share/gazebo-11/worlds
 export GAZEBO_MODEL_PATH=${GAZEBO_MODEL_PATH:+${GAZEBO_MODEL_PATH}:}${ICUB_INSTALL_PREFIX}/share/gazebo/models:${ICUB_INSTALL_PREFIX}/share/iCub/robots:${ICUB_INSTALL_PREFIX}/share
 
+'''
+
+rcFile=~/.bashrc
+
+echo -e "$rcadditions$rcadditions1" >> $rcFile
+
+fi
+
+
+if [ -z "${ICUB_TOOLDIR}" ] || [ "${ICUB_TOOLDIR}" != "${TOOLDIR}" ];
+then
+echo "Set model env variables in bashrc"
+cp ~/.bashrc $BASEDIR/.bashrc_prep_bak_$(date  "+%Y_%m_%d_%H_%M_%S")
+
+rcadditions="""
+export ICUB_TOOLDIR="$TOOLDIR"
+"""
+
+rcadditions1='''
 # individual models/worlds for Gazebo (Classic)
 export GAZEBO_MODEL_PATH=${GAZEBO_MODEL_PATH}:${ICUB_TOOLDIR}/gazebo_environment/object_models
 export GAZEBO_RESOURCE_PATH=${GAZEBO_RESOURCE_PATH}:${ICUB_TOOLDIR}/gazebo_environment/gazebo_config/worlds
